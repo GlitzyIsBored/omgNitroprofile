7b5d0a994bf003567573cfd9d1e1dea11d88df67
Mayesha <104177936+GlitzyIsBored@users.noreply.github.com>
2024-11-22 19:53:25 +1100

    src
    sd

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import Container from "./components/Container";
+import "./style.css";
+
+const App = () => {
+  return (
+    <div>
+      <h2 style={{ color: "white"}}>omg </h2>
+      <hr />
+      <Container />
+    </div>
+  );
+};
+
+export default App;
